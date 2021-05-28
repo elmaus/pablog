f298f3381db62cc6f39df331ca53844fad466024
Samuel Camus <elmauscamus@gmail.com>
2021-05-28 09:08:18 +0300

    nothing

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.http.response import HttpResponse
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Category
+
 
 def home(request):
     context = {
         'title':'Home',
-        'text':'This is a text'
+        'text':'This is a text',
+        'categories': Category.objects.all()
     }
     return render(request, 'blog/home.html', context)
